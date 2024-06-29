0390751f296800b30e63bf2ba99dc70d6945b0d5
Gabriel Martin <g.martin@htlkrems.at>
2024-06-29 10:17:28 +0200

    Setup aspirate k8s deployment

--- Mpm-Smart/Web.Host/Program.cs
+++ Mpm-Smart/Web.Host/Program.cs
@@ -2,7 +2,11 @@ using MpmSmart.Web.Host;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+// Configuration
+
 var launchProfile = builder.ExecutionContext.IsPublishMode ? "https" : "Watch";
+
+var publishToAzure = builder.ExecutionContext.IsPublishMode && builder.Configuration["azure"] == "true";
 
 var username = builder.AddParameter("username");
 var sqlPassword = builder.CreateStablePassword("sqlPassword");
@@ -13,13 +17,12 @@ var rabbitMqPassword = builder.CreateStablePassword("rabbitMqPassword");
 
 var redis = builder.AddRedis("redis")
     .WithPersistence()
-    .WithDataVolume()
-    .PublishAsAzureRedis();
+    .WithDataVolume();
 
-var sql = builder.AddSqlServer("sql", password: sqlPassword)
-    .WithDataVolume()
-    .PublishAsAzureSqlDatabase()
-    .AddDatabase("MPM-Smart");
+var sqlServer = builder.AddSqlServer("sql", password: sqlPassword)
+    .WithDataVolume();
+
+var sqlDb = sqlServer.AddDatabase("MPM-Smart");
 
 var rabbitMq = builder.AddRabbitMQ("rabbitmq", userName: username, password: rabbitMqPassword)
     .WithManagementPlugin()
@@ -28,15 +31,23 @@ var rabbitMq = builder.AddRabbitMQ("rabbitmq", userName: username, password: rab
 var kafka = builder.AddKafka("kafka")
     .WithDataVolume();
 
-var api = builder.AddProject<Projects.Web_Api>("Api", "Watch");
+var api = builder.AddProject<Projects.Web_Api>("Api", "Watch")
+    .WithHttpEndpoint();
 
 // Projects
 
 builder.AddProject<Projects.Web_Server>("Web-Server", "Watch")
-    .WithReference(api);
+    .WithReference(api)
+    .WithHttpEndpoint();
 
 var dbManager = builder.AddProject<Projects.DbManager>("dbmanager")
     .WithReference(redis)
-    .WithReference(sql);
+    .WithReference(sqlDb);
+
+if (publishToAzure)
+{
+    redis.PublishAsAzureRedis();
+    sqlServer.PublishAsAzureSqlDatabase();
+}
 
 await builder.Build().RunAsync();
