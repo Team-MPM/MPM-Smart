b98023d3d85cd25d0bdfa2ff73a7f46e514cef85
Gabriel Martin <g.martin@htlkrems.at>
2024-10-27 13:32:54 +0100

    Switch to XUnit

--- src/tests/ControllerTests/ControlProcess/ProgramTest.cs
+++ src/tests/ControllerTests/ControlProcess/ProgramTest.cs
@@ -1,49 +1,45 @@
 ﻿using System.Net;
+using System.Reflection;
 using Microsoft.Extensions.Logging;
 using Moq;
 using ControlProcess;
+using JetBrains.Annotations;
+using Xunit;
 
 namespace ControllerTests.ControlProcess;
 
-[TestFixture]
-[TestOf(typeof(Program))]
-public class ProgramTest
+[TestSubject(typeof(Program))]
+public class ProgramTest : IAsyncLifetime
 {
     private const string BackendExecutablePath = "../backend/backend.exe";
     private const string BackendProcessName = "Mpm-Smart-Backend-Test-Instance";
 
-    private ILogger m_Logger;
-    private IHttpClientFactory m_HttpClientFactory;
+    private readonly ILogger m_Logger = new Mock<ILogger>().Object;
+    private IHttpClientFactory m_HttpClientFactory = null!;
 
-    [OneTimeSetUp]
-    public void OneTimeSetUp()
+    public Task InitializeAsync()
     {
-        Directory.SetCurrentDirectory("../../../../../../build/controller/control-process");
+        Directory.SetCurrentDirectory(Path.Combine(Assembly.GetExecutingAssembly().Location, "../../../../../../../build/controller/control-process"));
         m_HttpClientFactory = HttpClientFactoryHelper.CreateHttpClientFactory("http://localhost:54321");
+        return Task.CompletedTask;
     }
 
-    [SetUp]
-    public void SetUp()
-    {
-        m_Logger = new Mock<ILogger>().Object;
-    }
+    public Task DisposeAsync() => Task.CompletedTask;
 
-    [Test]
+    [Fact]
     public void LaunchControlProcess_ShouldLaunchControlProcess()
     {
         var result = Program.LaunchControlProcess(m_Logger, BackendExecutablePath, BackendProcessName, _ => { });
-        Assert.That(result, Is.Not.Null);
+        Assert.NotNull(result);
     }
 
-    [Test]
+    [Fact]
     public async Task LaunchControlProcess_ShouldMonitorProcess()
     {
         var restartCallbackCallCount = 0;
 
-        Program.LaunchControlProcess(m_Logger, BackendExecutablePath, BackendProcessName, _ =>
-        {
-            restartCallbackCallCount++;
-        });
+        Program.LaunchControlProcess(m_Logger, BackendExecutablePath, BackendProcessName,
+            _ => { restartCallbackCallCount++; });
 
         var client = m_HttpClientFactory.CreateClient(HttpClientFactoryHelper.TestClientName);
 
@@ -54,25 +50,29 @@ public class ProgramTest
 
         Assert.Multiple(() =>
         {
-            Assert.That(response1.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-            Assert.That(response2.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-            Assert.That(restartCallbackCallCount, Is.GreaterThan(1));
+            Assert.Equal(HttpStatusCode.OK, response1.StatusCode);
+            Assert.Equal(HttpStatusCode.OK, response2.StatusCode);
+            Assert.Equal(2, restartCallbackCallCount);
         });
     }
 
-    [Test]
+    [Fact]
     public void LaunchControlProcess_DoesNotThrow()
     {
-        Assert.DoesNotThrow(() =>
+        try
         {
             Program.LaunchControlProcess(m_Logger, BackendExecutablePath, BackendProcessName, _ => { });
-        });
+        }
+        catch (Exception e)
+        {
+            Assert.Fail(e.Message);
+        }
     }
-    
-    [Test]
+
+    [Fact]
     public void CreateControlLogger_ShouldCreateControlLogger()
     {
         var result = Program.CreateControlLogger();
-        Assert.That(result, Is.Not.Null);
+        Assert.NotNull(result);
     }
 }
---
+++ src/tests/ControllerTests/RunInSeparateProcessAttribute.cs
@@ -0,0 +1,21 @@
+﻿// using NUnit.Framework.Interfaces;
+// using NUnit.Framework.Internal;
+// using NUnit.Framework.Internal.Commands;
+//
+// namespace ControllerTests;
+//
+// [AttributeUsage(AttributeTargets.Method)]
+// public class RunInSeparateProcessAttribute : NUnitAttribute, IWrapSetUpTearDown
+// {
+//     public TestCommand Wrap(TestCommand command) => new SeparateProcessCommand(command);
+// }
+//
+// public class SeparateProcessCommand(TestCommand innerCommand) : TestCommand(innerCommand.Test)
+// {
+//     public override TestResult Execute(TestExecutionContext context)
+//     {
+//         var process = Process.Start("nunit3-console.exe", $"{Test.FullName}.dll");
+//         process.WaitForExit();
+//         return context.CurrentResult;
+//     }
+// }
