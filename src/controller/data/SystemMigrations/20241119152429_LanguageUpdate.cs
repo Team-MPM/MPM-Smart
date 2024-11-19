ad787b7952ddeae1515683eef4ecb3c8512cb0b4
NotSayo <olafplazinski23@gmail.com>
2024-11-19 18:02:57 +0100

    fixed the database not displaying "Language" issue

--- src/controller/data/SystemMigrations/20241119152429_LanguageUpdate.cs
+++ src/controller/data/SystemMigrations/20241119152429_LanguageUpdate.cs
@@ -10,13 +10,19 @@ namespace Data.SystemMigrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.AddColumn<string>(
+                name: "Language",
+                table: "UserProfiles",
+                type: "TEXT",
+                nullable: true);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropColumn(
+                name: "Language",
+                table: "UserProfiles");
         }
     }
 }
