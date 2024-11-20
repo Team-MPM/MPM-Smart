1d2f593758c567ef633d9241f42654d9caeb4b3b
Gabriel Martin <g.martin@htlkrems.at>
2024-11-20 17:42:26 +0100

    Query BT Devices JS

--- src/frontend/wwwroot/js/app.js
+++ src/frontend/wwwroot/js/app.js
@@ -1,1 +1,23 @@
-﻿
+﻿function getBluetoothDevices() {
+    navigator.bluetooth.requestDevice({
+        acceptAllDevices: true,
+    }).then(device => {
+        console.log('Name: ' + device.name);
+        console.log('Id: ' + device.id);
+        return device;
+    }).then(device => {
+        return device.gatt.connect()
+    }).then(device => {
+        return device.gatt.connect();
+    }).then(server => {
+        return server.getPrimaryService('battery_service');
+    }).then(service => {
+        return service.getCharacteristic('battery_level');
+    }).then(characteristic => {
+        return characteristic.readValue();
+    }).then(value => {
+        console.log(`Battery percentage is ${value.getUint8(0)}`);
+    }).catch(error => {
+        console.error(error);
+    });
+}
