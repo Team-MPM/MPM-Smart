fa209893d8f4b641b8a626ef750d5d3eed0d7b54
Gabriel Martin <g.martin@htlkrems.at>
2024-11-23 15:06:23 +0100

    Documentation CI

--- src/frontend/wwwroot/js/app.js
+++ src/frontend/wwwroot/js/app.js
@@ -1,6 +1,6 @@
 ﻿function getBluetoothDevices() {
     navigator.bluetooth.requestDevice({
-        acceptAllDevices: true,
+        filters: [{ services: ["00000001-810e-4a5b-8d75-3e5b444bc3cf"] }]
     }).then(device => {
         console.log('Name: ' + device.name);
         console.log('Id: ' + device.id);
@@ -10,13 +10,13 @@
     }).then(device => {
         return device.gatt.connect();
     }).then(server => {
-        return server.getPrimaryService('battery_service');
+        return server.getPrimaryService('00000001-810e-4a5b-8d75-3e5b444bc3cf');
     }).then(service => {
-        return service.getCharacteristic('battery_level');
+        return service.getCharacteristic('00000002-810e-4a5b-8d75-3e5b444bc3cf');
     }).then(characteristic => {
         return characteristic.readValue();
     }).then(value => {
-        console.log(`Battery percentage is ${value.getUint8(0)}`);
+        console.log(`Data is ${value.getUint8(0)}`);
     }).catch(error => {
         console.error(error);
     });
