ee9f3042b1b0d4011bb29593b9974ee80e15062a
ForgottenTu <julian@favianis.com>
2024-12-10 14:12:39 +0100

    DeviceOptions

---
+++ src/controller/api_schema/DeviceOptions/DeviceOption.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiSchema;
+
+public class DeviceOption
+{
+    public string Name { get; set; }
+    public EType Type { get; set; }
+    public int MaxValue { get; set; }
+    public int MinValue { get; set; }
+    public int DefaultValue { get; set; }
+}
+
+
+public enum EType {Radio, Slider, Checkbox}
