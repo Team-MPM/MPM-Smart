a56ebca543cf30103d65306cbb33d610424c67ff
Gabriel Martin <g.martin@htlkrems.at>
2024-12-14 11:03:18 +0100

    Rework plugin dev workflow

---
+++ src/build-tasks/ReadJsonFileTask/ReadPluginJsonFile.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Build.Framework;
+using Newtonsoft.Json.Linq;
+using Task = Microsoft.Build.Utilities.Task;
+
+namespace ReadJsonFileTask;
+
+public class ReadPluginJsonFile : Task
+{
+    [Required]
+    public required string JsonFilePath { get; set; }
+
+    [Output]
+    public string? Name { get; set; }
+
+    [Output]
+    public string? RegistryName { get; set; }
+
+    [Output]
+    public string? Description { get; set; }
+
+    [Output]
+    public string? Author { get; set; }
+
+    [Output]
+    public string? Version { get; set; }
+
+    public override bool Execute()
+    {
+        try
+        {
+            var json = File.ReadAllText(JsonFilePath);
+            var jsonObject = JObject.Parse(json);
+
+            Name = jsonObject[nameof(Name)]?.ToString();
+            RegistryName = jsonObject[nameof(RegistryName)]?.ToString();
+            Description = jsonObject[nameof(Description)]?.ToString();
+            Author = jsonObject[nameof(Author)]?.ToString();
+            Version = jsonObject[nameof(Version)]?.ToString();
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Log.LogErrorFromException(ex);
+            return false;
+        }
+    }
+}
---
+++ src/controller/plugins/PluginTemplate/Endpoints/TestEndpoints.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace MpmSmart.PluginTemplate.Endpoints;
+
+public static class TestEndpoints
+{
+    public static void MapTestEndpoints(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapGet("/plugins/test-plugin", (HttpContext context) =>
+        {
+            return "Hello, World from Test Plugin!";
+        });
+    }
+}
