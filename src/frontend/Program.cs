159121972e864583a766e388e87e9dcf742ebe4d
NotSayo <olafplazinski23@gmail.com>
2024-12-17 19:53:02 +0100

    Fixed the duplicate Mudblazorprividers issue, implemented auth in Frontend, made [Authorize] work

--- src/frontend/Program.cs
+++ src/frontend/Program.cs
@@ -1,17 +1,23 @@
-using ApiSchema.DeviceOptions;
-using Data.System;
+using BackendConnectionData.Services;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Frontend;
+using Microsoft.AspNetCore.Components.Authorization;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-builder.Services.AddMudServices();
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<CustomAuthStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(provider => provider.GetRequiredService<CustomAuthStateProvider>());
+builder.Services.AddAuthorizationCore();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddMudServices();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:543/") });
 
 await builder.Build().RunAsync();
